c28cc0601cfacaf8ae7dc836c310933477d0272e
moi15moi <moi15moismokerlolilol@gmail.com>
2022-12-21 17:28:22 -0500

    Correct variable name

--- ass_tag_parser/ass_item/ass_tag_clip.py
+++ ass_tag_parser/ass_item/ass_tag_clip.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 @dataclass
 class AssTagClipRectangle(AssTag):
     # https://github.com/libass/libass/blob/44f6532daf5eb13cb1aa95f5449a77b5df1dd85b/libass/ass_parse.c#L685-L704
-    inverse: bool
+    invert: bool
 
     @property
     def tag(self) -> str:
-        if self.inverse:
+        if self.invert:
             return "iclip"
         return "clip"
 
