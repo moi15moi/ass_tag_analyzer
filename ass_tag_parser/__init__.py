7fd39ab0f12d471991c1d29f1cd44dd2c7e579ad
moi15moi <moi15moismokerlolilol@gmail.com>
2022-12-23 21:25:09 -0500

    Update __init__.py

--- ass_tag_parser/__init__.py
+++ ass_tag_parser/__init__.py
@@ -1,14 +1,4 @@
-# from .ass_composer import compose_ass
-# from .ass_parser import ass_to_plaintext, parse_ass
-# from ass_tag_parser import ass_item
-# from .ass_parser import parse_ass, tags_to_line
-# from .ass_type_parser import *
-
-# from .draw_composer import compose_draw_commands
-# from .draw_parser import parse_draw_commands
-# from .draw_struct import *
-
 from .ass_item import *
-from .ass_parser import parse_ass, parse_tags
+from .ass_parser import parse_ass, parse_tags, tags_to_text
 
 __version__ = "0.0.1"
