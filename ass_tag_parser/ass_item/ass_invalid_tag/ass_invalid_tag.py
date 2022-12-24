cf639c975bc211b794b095771ba4c343ad8f4d87
moi15moi <moi15moismokerlolilol@gmail.com>
2022-12-23 21:43:06 -0500

    Add some documentation about AssInvalidTag

--- ass_tag_parser/ass_item/ass_invalid_tag/ass_invalid_tag.py
+++ ass_tag_parser/ass_item/ass_invalid_tag/ass_invalid_tag.py
@@ -4,6 +4,18 @@ from dataclasses import dataclass
 
 @dataclass
 class AssInvalidTag(AssTag):
+    """
+    An AssInvalidTag is used when ass_tag_parser cannot know what is the value of an tag because it depend of the style of the line.
+    Ex:
+        "{\\i}Example" would return an AssInvalidTagItalic, because the text "Example" will totally depend on the style of the line.
+        So, if the style is italic, then "Example" will be display in italic and vice-versa.
+
+    Attributes:
+        text (str): The text of an invalid tag. At 99% of the time, the text will be empty.
+            Ex:
+                "\\b99" would return AssInvalidTagBold("99") instanc which is a AssInvalidTag.
+    """
+
     text: str
 
     def __str__(self):
